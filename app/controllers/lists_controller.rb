0b949ec1b30c78b5b97b826ea9c0d41797268bfa
NunoH4 <132256457+NunoH4@users.noreply.github.com>
2023-05-08 08:29:35 +0000

    フラッシュメッセージ実装

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -4,14 +4,12 @@ class ListsController < ApplicationController
   end
 
   def create
-    # 1.&2. データを受け取り新規登録するためのインスタンス作成
     list = List.new(list_params)
-    # 3. データをデータベースに保存するためのsaveメソッド実行
     list.save
-    # 4. トップ画面へリダイレクト
+    flash[:notice] = "投稿が成功しました"
     redirect_to list_path(list.id)
   end
-  
+
   def index
     @lists = List.all
   end
@@ -23,29 +21,20 @@ class ListsController < ApplicationController
   def edit
     @list = List.find(params[:id])
   end
-  
+
   def update
     list = List.find(params[:id])
     list.update(list_params)
     redirect_to list_path(list.id)
   end
-  
+
   def destroy
     list = List.find(params[:id])
     list.destroy
     redirect_to '/lists'
   end
-  
-  def create
-    @list = List.new(list_params)
-    if @list.save
-      redirect_to list_path(@list.id)
-    else
-      render :new
-    end  
-  end
-  
- 
+
+
   private
   def list_params
     params.require(:list).permit(:title, :body, :image)
